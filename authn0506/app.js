3a8ddc1969088c95c67f8e33cfdd548523668a77
Felidae922 <102779773+s20010922@users.noreply.github.com>
2023-05-07 01:07:34 +0800

    version3

--- authn0506/app.js
+++ authn0506/app.js
@@ -1,61 +1,41 @@
 const createCredentialOptions = {
-  publicKey: {
-    rp: {
-      name: 'Yubico WebAuthn Demo',
-    },
-    user: {
-      id: new Uint8Array([/* 填寫用戶的唯一識別 */]),
-      name: 'user@example.com',
-      displayName: 'User',
-    },
-    pubKeyCredParams: [
-      {
-        type: 'public-key',
-        alg: -7, // 使用 ES256 算法
-      },
-    ],
-    timeout: 60000,
-    challenge: new Uint8Array([/* 生成一個隨機的挑戰值 */]),
-  },
-};
-
-const requestCredentialOptions = {
-  publicKey: {
-    timeout: 60000,
-    challenge: new Uint8Array([/* 生成一個隨機的挑戰值 */]),
-    allowCredentials: [
-      {
-        type: 'public-key',
-        id: new Uint8Array([/* 填寫註冊時返回的憑證ID */]),
-      },
-    ],
-  },
+    publicKey: {
+        challenge: new Uint8Array([/* 生成一個隨機的挑戰值 */]),
+        rp: {
+            name: 'WebAuthn Demo',
+        },
+        user: {
+            id: new Uint8Array([/* 填寫用戶的唯一識別 */]),
+            name: 'user@example.com',
+            displayName: 'User',
+        },
+        pubKeyCredParams: [
+            {
+                type: 'public-key',
+                alg: -7, // 表示使用ES256算法
+            }
+        ],
+        authenticatorSelection: {
+            authenticatorAttachment: 'platform',
+        },
+        attestation: 'direct',
+        timeout: 60000,
+    }
 };
 
 async function register() {
-  try {
-    const credential = await navigator.credentials.create(createCredentialOptions);
-    console.log('Credential created:', credential);
-    showMessage('Registration successful!');
-  } catch (error) {
-    console.error(error);
-    showMessage(`Registration failed: ${error.message}`);
-  }
-}
-
-async function login() {
-  try {
-    const credential = await navigator.credentials.get(requestCredentialOptions);
-    console.log('Credential retrieved:', credential);
-    showMessage('Login successful!');
-  } catch (error) {
-    console.error(error);
-    showMessage(`Login failed: ${error.message}`);
-  }
+    try {
+        const credential = await navigator.credentials.create(createCredentialOptions);
+        console.log('Credential created:', credential);
+        showMessage('Registration successful!');
+    } catch (error) {
+        console.error(error);
+        showMessage(`Registration failed: ${error.message}`);
+    }
 }
 
 function showMessage(message) {
-  const messageDiv = document.getElementById('message');
-  messageDiv.textContent = message;
-  messageDiv.style.display = 'block';
+    const messageDiv = document.getElementById('message');
+    messageDiv.textContent = message;
+    messageDiv.style.display = 'block';
 }
